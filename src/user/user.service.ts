5f0f3453048b42b6af39eae939d4406307ca6d53
wff <283953455@qq.com>
2024-08-31 15:27:20 +0800

    feat: 用户注册

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
 import RegisterDto from "./dto/register.dto";
+import { hash } from "argon2";
+import { PrismaService } from "@/prisma/prisma.service";
+
 @Injectable()
 export class UserService {
+  @Inject()
+  private readonly prisma: PrismaService;
+
   async register(user: RegisterDto) {
-    console.log("user", user);
+    const password = await hash(user.password);
+    return await this.prisma.user.create({
+      data: {
+        name: user.name,
+        password
+      }
+    });
   }
 }
